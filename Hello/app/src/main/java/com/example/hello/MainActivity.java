2bb9572bac001fdf76cfca1423d45bf83d693985
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2019-03-01 03:07:30 +0600

    Added Fragments

--- Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.hello;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdaptor myTabsAccessorAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Hello");
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdaptor = new TabsAccessorAdaptor(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdaptor);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 }
