12a9a8e68246bbe9840499c154b746af576ab204
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2019-03-02 05:09:58 +0600

    Project Restarted

--- Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -1,54 +1,13 @@
 package com.example.hello;
 
-import android.content.Intent;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Toolbar mToolbar;
-    private ViewPager myViewPager;
-    private TabLayout myTabLayout;
-    private TabsAccessorAdaptor myTabsAccessorAdaptor;
-
-    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Hello");
-
-        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
-        myTabsAccessorAdaptor = new TabsAccessorAdaptor(getSupportFragmentManager());
-        myViewPager.setAdapter(myTabsAccessorAdaptor);
-
-        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
-        myTabLayout.setupWithViewPager(myViewPager);
-    }
-
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-
-        if(currentUser == null)
-        {
-            sendUserToLoginActivity();
-        }
-    }
-
-    private void sendUserToLoginActivity()
-    {
-        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(loginIntent);
     }
 }
