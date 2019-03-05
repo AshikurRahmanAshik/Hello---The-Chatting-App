4033da04917d2b80fa7a225a6f5791ac1958e515
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2019-03-05 11:06:55 +0600

    08 Already have Account And Need new Account Link

--- Hello/app/src/main/java/com/example/hello/LoginActivity.java
+++ Hello/app/src/main/java/com/example/hello/LoginActivity.java
@@ -3,18 +3,45 @@ package com.example.hello;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity
 {
     private FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEmail,UserPassword;
+    private TextView NeedNewAccountLink, ForgetPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Initializefields();
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void Initializefields()
+    {
+        LoginButton = (Button) findViewById(R.id.login_button);
+        PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        ForgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
     }
 
     @Override
@@ -31,5 +58,11 @@ public class LoginActivity extends AppCompatActivity
     {
         Intent loginIntend = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntend);
+    }
+
+    private void sendUserToRegisterActivity()
+    {
+        Intent registerIntend = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntend);
     }
 }
