bf6ca5a461da8c8f633f41e5025519c35ec3eb0b
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2019-03-05 16:18:09 +0600

    09 Android Login and Registration with Firebase

--- Hello/app/src/main/java/com/example/hello/RegisterActivity.java
+++ Hello/app/src/main/java/com/example/hello/RegisterActivity.java
@@ -1,12 +1,21 @@
 package com.example.hello;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity
 {
@@ -14,13 +23,17 @@ public class RegisterActivity extends AppCompatActivity
     private EditText UserEmail,UserPassword;
     private TextView AlreadyHaveAccountLink;
 
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) 
+    protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
         Initializefields();
 
@@ -32,6 +45,54 @@ public class RegisterActivity extends AppCompatActivity
             }
         });
 
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
+
+    }
+
+    private void CreateNewAccount()
+    {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please insert email!", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please insert password!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Creating new account!");
+            loadingBar.setMessage("Please wait!");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(task.isSuccessful())
+                    {
+                        sendUserToLoginActivity();
+                        Toast.makeText(RegisterActivity.this, "Account created succesfully!", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                    else
+                    {
+                        String message = task.getException().toString();
+                        Toast.makeText(RegisterActivity.this, "Error: "+message, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
     }
 
     private void Initializefields()
@@ -40,6 +101,7 @@ public class RegisterActivity extends AppCompatActivity
         UserEmail = (EditText) findViewById(R.id.register_email);
         UserPassword = (EditText) findViewById(R.id.register_password);
         AlreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+        loadingBar = new ProgressDialog(this);
     }
 
     private void sendUserToLoginActivity()
