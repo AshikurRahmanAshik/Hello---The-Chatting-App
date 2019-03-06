3e80579b25fbabe7045fcf34fb329c2f25c5cad1
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2019-03-06 13:36:11 +0600

    16 - Half Done

--- Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.hello;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -109,12 +113,50 @@ public class MainActivity extends AppCompatActivity
         {
             sendUserToSettingsActivity();
         }
+        if(item.getItemId() == R.id.main_create_group_option)
+        {
+            RequestNewGroup();
+        }
         if(item.getItemId() == R.id.main_find_friends_option)
         {
 
         }
         return true;
     }
+
+    private void RequestNewGroup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialog);
+        builder.setTitle("Enter Group Name: ");
+
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("e.g: Cool bros!");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String groupName = groupNameField.getText().toString();
+
+                if(TextUtils.isEmpty(groupName))
+                {
+                    Toast.makeText(MainActivity.this, "Please Write Group Name..", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+    }
+
     private void sendUserToLoginActivity()
     {
         Intent loginIntend = new Intent(MainActivity.this, LoginActivity.class);
