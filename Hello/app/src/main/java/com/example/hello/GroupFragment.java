467d5e13c4a0443fbcf19398e8076fdd39f06491
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2019-03-06 16:49:31 +0600

    17 retrieve data on listview from firebase realtime database

--- Hello/app/src/main/java/com/example/hello/GroupFragment.java
+++ Hello/app/src/main/java/com/example/hello/GroupFragment.java
@@ -2,17 +2,36 @@ package com.example.hello;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GroupFragment extends Fragment {
-
+public class GroupFragment extends Fragment
+{
+    private View groupFragmentView;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_groups = new ArrayList<>();
+    private DatabaseReference GroupRef;
 
     public GroupFragment() {
         // Required empty public constructor
@@ -21,9 +40,46 @@ public class GroupFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false);
+                             Bundle savedInstanceState)
+    {
+        groupFragmentView = inflater.inflate(R.layout.fragment_group, container, false);
+
+        GroupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
+        InitializedFields();
+
+        RetrieveAndDisplayGroups();
+        return groupFragmentView;
     }
 
+    private void InitializedFields()
+    {
+        listView = (ListView) groupFragmentView.findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,list_of_groups);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    private void RetrieveAndDisplayGroups()
+    {
+        GroupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                Set<String> set = new HashSet<>();
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+
+                while (iterator.hasNext())
+                {
+                    set.add(((DataSnapshot) iterator.next()).getKey());
+                }
+                list_of_groups.clear();
+                list_of_groups.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
